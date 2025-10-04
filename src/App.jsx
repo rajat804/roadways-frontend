b3f88e28941d643d7728a152a165cb5e6e592f54
bobbygour30 <bobby30gour@gmail.com>
2025-10-04 19:37:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage'
-import Footer from './components/Footer'
-import { Routes, Route } from 'react-router-dom'
-import About from './pages/About'
-import Services from './pages/Services'
-import Career from './pages/Career'
-import ContactUs from './pages/Contact'
-import TrackConsignment from './pages/Track'
-import PickupRequest from './pages/Pickup'
-import Network from './pages/Network'
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
+import Career from './pages/Career';
+import ContactUs from './pages/Contact';
+import TrackConsignment from './pages/Track';
+import PickupRequest from './pages/Pickup';
+import Network from './pages/Network';
+import ScrollToTop from './components/ScrollToTop'; // ✅ Import here
 
 const App = () => {
   return (
     <div>
-      
       <Navbar />
+      {/* ✅ This ensures page scrolls to top on every route change */}
+      <ScrollToTop />
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
@@ -27,9 +29,10 @@ const App = () => {
         <Route path="/pickup" element={<PickupRequest />} />
         <Route path="/network" element={<Network />} />
       </Routes>
+
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
